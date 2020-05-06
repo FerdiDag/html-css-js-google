c652d9955545f1da1edf495aac50e1d9ea3183cc
Ferdinando <ferdi.dagostino@gmail.com>
2020-05-06 13:34:20 +0200

    push

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ $('.fa-grip-vertical').click(function() {
     $('.dropdown').toggle();
 
 });
+// $('#main-menu > li > a').click(function() {
+//     var sottomenu = $(this).next('.dropdown');
+//     sottomenu.toggle();
+// })
